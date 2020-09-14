8cc618408af573f50fbe97eb3636af4ad6f1d21b
Dmitry Dimov <goden4you@Mac-mini-Dmitry.local>
2020-09-15 00:33:34 +0300

    without working

---
+++ app/src/components/musicInfo/FileInfo.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+// TODO данные взять из редакса
+
+export const FileInfo = ({}) => {
+  const {
+    trackPlayerInit,
+    minimazed,
+    tracksTitles,
+    tracksAuthors,
+    trackId,
+    firstTrackId,
+  } = this.state;
+  return trackPlayerInit ? (
+    <View style={minimazed ? styles.trackInfoMinimazed : styles.trackInfo}>
+      <Text
+        style={
+          minimazed
+            ? [styles.largeTextMinimazed, styles.trackInfoTextMinimazed]
+            : [styles.largeText, styles.trackInfoText]
+        }>
+        {tracksTitles[trackId - firstTrackId]}
+      </Text>
+      <Text
+        style={
+          minimazed
+            ? [styles.smallTextMinimazed, styles.trackInfoTextMinimazed]
+            : [styles.smallText, styles.trackInfoText]
+        }>
+        {tracksAuthors[trackId - firstTrackId]}
+      </Text>
+    </View>
+  ) : null;
+};
+
+const styles = StyleSheet.create({
+  trackInfoMinimazed: {
+    width: '70%',
+    paddingLeft: 10,
+    textAlign: 'left',
+  },
+  trackInfo: {
+    padding: 20,
+    width: '100%',
+  },
+  trackInfoText: {
+    textAlign: 'center',
+    flexWrap: 'wrap',
+    fontFamily: 'HouschkaPro-Medium',
+  },
+  trackInfoTextMinimazed: {
+    textAlign: 'left',
+    flexWrap: 'wrap',
+    fontFamily: 'HouschkaPro-Medium',
+  },
+  largeTextMinimazed: {
+    fontSize: 18,
+  },
+  smallTextMinimazed: {
+    fontSize: 14,
+    color: 'rgb(147, 149, 152)',
+  },
+  largeText: {
+    fontSize: 22,
+  },
+  smallText: {
+    fontSize: 16,
+    color: 'rgb(147, 149, 152)',
+  },
+});
